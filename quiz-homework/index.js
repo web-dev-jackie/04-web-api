d5b1a176ba500637f22a2b1a351b5c15014715d8
Jackie <heatleyj7@icloud.com>
2022-12-02 23:27:57 -0500

    Adding countdown.

--- quiz-homework/index.js
+++ quiz-homework/index.js
@@ -1,91 +1,107 @@
+const { SECONDS } = require("countdown");
+
 // var startButton = document.querySelector("#generate");
-var selectA = document.querySelector("#a")
-var selectB = document.querySelector("#b")
-var selectC = document.querySelector("#c")
-var selectD = document.querySelector("#d")
+var selectA = document.querySelector("#a");
+var selectB = document.querySelector("#b");
+var selectC = document.querySelector("#c");
+var selectD = document.querySelector("#d");
 
-startBox = document.getElementById("start-box")
-startButton = document.getElementById("start-button")
-quizBox = document.getElementById("quiz-box")
-question = document.getElementById("question")
+startBox = document.getElementById("start-box");
+startButton = document.getElementById("start-button");
+quizBox = document.getElementById("quiz-box");
+question = document.getElementById("question");
 
-var answers = ["A", "B", "C", "D"]
 var userChoice = "";
-var time;
 var addTime;
 
+        var timerId = countdown(
+          new Date(),
+          function (ts) {
+            document.getElementById("pageTimer").innerHTML =
+              ts.toHTML("strong");
+          },
+          countdown.HOURS | countdown.MINUTES | countdown.SECONDS
+        ); 
+
+// later on this timer may be stopped
+window.clearInterval(timerId);
+
 const questions = [
-    {
-        question: "First Question",
-        answer1: "answer one",
-        answer2: "answer two",
-        answer3: "answer three",
-        answer4: "answer four",
-        
-        correct: "A"
-    },
-    {
-        question: "Second Question",
-        answer1: "answer one",
-        answer2: "answer two",
-        answer3: "answer three",
-        answer4: "answer four",
-        
-        correct: "B"
-    },
-    {
-        question: "Third Question",
-        answer1: "answer one",
-        answer2: "answer two",
-        answer3: "answer three",
-        answer4: "answer four",
-        
-        correct: "C"
-    },
-    {
-        question: "Fourth Question",
-        answer1: "answer one",
-        answer2: "answer two",
-        answer3: "answer three",
-        answer4: "answer four",
-        
-        correct: "D"
-    },
-    {
-        question: "Fifth Question",
-        answer1: "answer one",
-        answer2: "answer two",
-        answer3: "answer three",
-        answer4: "answer four",
-        
-        correct: "A"
-    }
-]
+  {
+    question: "First Question",
+    answer1: "answer one",
+    answer2: "answer two",
+    answer3: "answer three",
+    answer4: "answer four",
+
+    correct: "A",
+  },
+  {
+    question: "Second Question",
+    answer1: "answer one",
+    answer2: "answer two",
+    answer3: "answer three",
+    answer4: "answer four",
+
+    correct: "B",
+  },
+  {
+    question: "Third Question",
+    answer1: "answer one",
+    answer2: "answer two",
+    answer3: "answer three",
+    answer4: "answer four",
+
+    correct: "C",
+  },
+  {
+    question: "Fourth Question",
+    answer1: "answer one",
+    answer2: "answer two",
+    answer3: "answer three",
+    answer4: "answer four",
+
+    correct: "D",
+  },
+  {
+    question: "Fifth Question",
+    answer1: "answer one",
+    answer2: "answer two",
+    answer3: "answer three",
+    answer4: "answer four",
+
+    correct: "A",
+  },
+];
+
+
+
+
+
 function zero() {
-    question.textContent = questions[0].question
+  question.textContent = questions[0].question;
 }
 function one() {
-question.textContent = questions[1].question
+  question.textContent = questions[1].question;
 }
 function two() {
-    question.textContent = questions[2].question
+  question.textContent = questions[2].question;
 }
 function three() {
-    question.textContent = questions[3].question
+  question.textContent = questions[3].question;
 }
 function four() {
-    question.textContent = questions[4].question
+  question.textContent = questions[4].question;
 }
-// 
+//
 
 function init() {
-    getHigh();
+  getHigh();
 }
-
 
 function startQuiz() {
-    startBox.style.display = "none"
-    quizBox.style.display = "block"
+  startBox.style.display = "none";
+  quizBox.style.display = "block";
 }
 
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startQuiz);
